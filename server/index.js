235b539f8eeeee6955b4d71b15c60e0c9a5dda9e
yoea13 <yoea13@gmail.com>
2023-02-28 13:25:59 -0300

    au course

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "04b12bd1410b983d4d94998e400ec4c19e0bef0f983cbbea3310f6148c67d087b7132511c1474cf0f1a7149427b627be449a833d64824df750a9d70945aea3d9ba": 100,
+  "0403628b6dce6a37f186b59a503d274f0aab55a7b13f39857c0c34d1759c4821750d61e6a3d0f1069b5b1b57135c688beb39906e941cede0df8e851c284277ae97": 50,
+  "044c9b16e975b8a7e714d6367bae79bd248abee8428eeae6d170d6bed595910a647bac2ec57d85275d6fe0ff452b9b081417b74a1c6ea651ec2f8a14cd62ba73e5": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +19,10 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+  // TODO: get a signature from the client-side app
+  // recover the public address from the signature
+  // ESE va a ser el sender (variable)
+
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
